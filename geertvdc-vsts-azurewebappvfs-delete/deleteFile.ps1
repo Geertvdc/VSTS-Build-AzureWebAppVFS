c529635c3bbd084da78df8eceeb1a72e5b3246c7
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-02-13 13:00:39 +0100

    added option to continue on 404 error message

--- geertvdc-vsts-azurewebappvfs-delete/deleteFile.ps1
+++ geertvdc-vsts-azurewebappvfs-delete/deleteFile.ps1
@@ -5,19 +5,21 @@ $WebAppName = Get-VstsInput -Name WebAppName -Require
 $filePath = Get-VstsInput -Name filePath -Require
 $allowUnsafe = Get-VstsInput -Name allowUnsafe
 $alternativeKuduUrl = Get-VstsInput -Name alternativeKuduUrl 
+$continueIfFileNotExist = Get-VstsInput -Name continueIfFileNotExist 
+
 
 # Initialize Azure.
 Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
 Initialize-Azure
 
-Write-VstsTaskVerbose "Azure Initialized"
+Write-Output "Azure Initialized"
 
 #Import-Module $PSScriptRoot\vfs
 
 $webapp = Get-AzureRmWebApp -name "$WebAppName"
 $resourceGroup = $webapp.ResourceGroup
 
-Write-VstsTaskVerbose "Retrieved web app: $webapp in Resource group: $resourceGroup"
+Write-Output "Retrieved web app: $webapp in Resource group: $resourceGroup"
 
 function Get-AzureRmWebAppPublishingCredentials($resourceGroupName, $webAppName, $slotName = $null){
 	if ([string]::IsNullOrWhiteSpace($slotName)){
@@ -32,34 +34,40 @@ function Get-AzureRmWebAppPublishingCredentials($resourceGroupName, $webAppName,
     	return $publishingCredentials
 }
 
-function Get-KuduApiAuthorisationToken($username, [securestring]$password){
+function Get-KuduApiAuthorisationToken($username, $password){
     return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password))))
 }
 
-Write-VstsTaskVerbose "Retrieving publishing profile"
+Write-Output "Retrieving publishing profile"
 $login = Get-AzureRmWebAppPublishingCredentials "$resourceGroup" "$WebAppName"
-Write-VstsTaskVerbose "Publishing profile retrieved"
+Write-Output "Publishing profile retrieved"
 
-function Remove-FileFromWebApp($webAppName, $slotName = "", $username, [securestring]$password, $filePath, $allowUnsafe = $false, $alternativeUrl){
+function Remove-FileFromWebApp($webAppName, $slotName = "", $username, $password, $filePath, $allowUnsafe = $false, $alternativeUrl, $continueIfFileNotExist){
 
-		Write-VstsTaskVerbose "user $username"
-		Write-VstsTaskVerbose "webapp $webAppName"
-		Write-VstsTaskVerbose "path $filePath"
+		Write-Output "user $username"
+		Write-Output "webapp $webAppName"
+		Write-Output "path $filePath"
+		Write-Output "allowunsafe $allowUnsafe"
 
     $kuduApiAuthorisationToken = Get-KuduApiAuthorisationToken $username $password
-    if ($slotName -eq ""){
+    if ($slotName -eq ""){				
         $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
+				Write-Output "slotname empty deploying to $kuduApiUrl"
     }
     else{
         $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
+				Write-Output "slotname not empty deploying to $kuduApiUrl"
     }
 
 		if($alternativeUrl -ne ""){
+			  Write-Output "replacing scm.azurewebsites.net by $alternativeUrl"
 				$kuduApiUrl = $kuduApiUrl.Replace("scm.azurewebsites.net","$alternativeUrl")
 		}
 
-		Write-VstsTaskVerbose "url $kuduApiUrl"
-    	if($allowUnsafe){
+		Write-Output "url $kuduApiUrl"
+
+    if($allowUnsafe -eq $true){
+			Write-Output "adding ignore for self signed certificates"
 	 		add-type @"
 	   		using System.Net;
 	   		using System.Security.Cryptography.X509Certificates;
@@ -74,12 +82,27 @@ function Remove-FileFromWebApp($webAppName, $slotName = "", $username, [securest
 			[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 		}
 
-		Invoke-RestMethod -Uri $kuduApiUrl `
+		try {
+			Invoke-RestMethod -Uri $kuduApiUrl `
 												-Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} `
 												-Method DELETE `
-												-ContentType "multipart/form-data"
+												-ContentType "multipart/form-data"			
+		}
+		catch {
+			if($_.Exception.Response.StatusCode.value__ -eq "404" -and $continueIfFileNotExist -eq $true){
+				Write-Output "File not found"
+			}
+			else {
+				throw $_.Exception
+			}
+			
+		}
+
 }
 
-Write-VstsTaskVerbose "Deleting file from Web App"
-Remove-FileFromWebApp -webAppName "$WebAppName" -username $login.Properties.PublishingUserName -password $login.Properties.PublishingPassword -filePath "$filePath" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeKuduUrl
-Write-VstsTaskVerbose"File $filePath deleted"
+#$pw = ConvertTo-SecureString $login.Properties.PublishingPassword -AsPlainText -Force 
+$pw = $login.Properties.PublishingPassword
+
+Write-Output "Deleting file from Web App"
+Remove-FileFromWebApp -webAppName "$WebAppName" -username $login.Properties.PublishingUserName -password $pw -filePath "$filePath" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeKuduUrl -continueIfFileNotExist $continueIfFileNotExist
+Write-Output "File $filePath deleted"
