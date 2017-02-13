48074ec05cfba5bf92ace40c18bf998921447bea
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-02-13 16:10:23 +0100

    added recursive delete of dirs

--- geertvdc-vsts-azurewebappvfs-delete/deleteFile.ps1
+++ geertvdc-vsts-azurewebappvfs-delete/deleteFile.ps1
@@ -3,6 +3,7 @@ Trace-VstsEnteringInvocation $MyInvocation
 $ConnectedServiceName = Get-VstsInput -Name ConnectedServiceName -Require
 $WebAppName = Get-VstsInput -Name WebAppName -Require
 $filePath = Get-VstsInput -Name filePath -Require
+$deleteRecursive = Get-VstsInput -Name deleteRecursive 
 $allowUnsafe = Get-VstsInput -Name allowUnsafe
 $alternativeKuduUrl = Get-VstsInput -Name alternativeKuduUrl 
 $continueIfFileNotExist = Get-VstsInput -Name continueIfFileNotExist 
@@ -11,98 +12,21 @@ $continueIfFileNotExist = Get-VstsInput -Name continueIfFileNotExist
 # Initialize Azure.
 Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
 Initialize-Azure
-
 Write-Output "Azure Initialized"
 
-#Import-Module $PSScriptRoot\vfs
+Import-Module $PSScriptRoot\vfs
+Write-Output "VFS scripts Initialized"
 
 $webapp = Get-AzureRmWebApp -name "$WebAppName"
 $resourceGroup = $webapp.ResourceGroup
 
 Write-Output "Retrieved web app: $webapp in Resource group: $resourceGroup"
-
-function Get-AzureRmWebAppPublishingCredentials($resourceGroupName, $webAppName, $slotName = $null){
-	if ([string]::IsNullOrWhiteSpace($slotName)){
-		$resourceType = "Microsoft.Web/sites/config"
-		$resourceName = "$webAppName/publishingcredentials"
-	}
-	else{
-		$resourceType = "Microsoft.Web/sites/slots/config"
-		$resourceName = "$webAppName/$slotName/publishingcredentials"
-	}
-	$publishingCredentials = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -Action list -ApiVersion 2015-08-01 -Force
-    	return $publishingCredentials
-}
-
-function Get-KuduApiAuthorisationToken($username, $password){
-    return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password))))
-}
-
 Write-Output "Retrieving publishing profile"
 $login = Get-AzureRmWebAppPublishingCredentials "$resourceGroup" "$WebAppName"
 Write-Output "Publishing profile retrieved"
 
-function Remove-FileFromWebApp($webAppName, $slotName = "", $username, $password, $filePath, $allowUnsafe = $false, $alternativeUrl, $continueIfFileNotExist){
-
-		Write-Output "user $username"
-		Write-Output "webapp $webAppName"
-		Write-Output "path $filePath"
-		Write-Output "allowunsafe $allowUnsafe"
-
-    $kuduApiAuthorisationToken = Get-KuduApiAuthorisationToken $username $password
-    if ($slotName -eq ""){				
-        $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
-				Write-Output "slotname empty deploying to $kuduApiUrl"
-    }
-    else{
-        $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
-				Write-Output "slotname not empty deploying to $kuduApiUrl"
-    }
-
-		if($alternativeUrl -ne ""){
-			  Write-Output "replacing scm.azurewebsites.net by $alternativeUrl"
-				$kuduApiUrl = $kuduApiUrl.Replace("scm.azurewebsites.net","$alternativeUrl")
-		}
-
-		Write-Output "url $kuduApiUrl"
-
-    if($allowUnsafe -eq $true){
-			Write-Output "adding ignore for self signed certificates"
-	 		add-type @"
-	   		using System.Net;
-	   		using System.Security.Cryptography.X509Certificates;
-		 		public class TrustAllCertsPolicy : ICertificatePolicy {
-		   		public bool CheckValidationResult(
-			 		ServicePoint srvPoint, X509Certificate certificate,
-			 		WebRequest request, int certificateProblem) {
-			   		return true;
-			 		}
-		 		}
-"@
-			[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-		}
-
-		try {
-			Invoke-RestMethod -Uri $kuduApiUrl `
-												-Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} `
-												-Method DELETE `
-												-ContentType "multipart/form-data"			
-		}
-		catch {
-			if($_.Exception.Response.StatusCode.value__ -eq "404" -and $continueIfFileNotExist -eq $true){
-				Write-Output "File not found"
-			}
-			else {
-				throw $_.Exception
-			}
-			
-		}
-
-}
-
-#$pw = ConvertTo-SecureString $login.Properties.PublishingPassword -AsPlainText -Force 
 $pw = $login.Properties.PublishingPassword
 
 Write-Output "Deleting file from Web App"
-Remove-FileFromWebApp -webAppName "$WebAppName" -username $login.Properties.PublishingUserName -password $pw -filePath "$filePath" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeKuduUrl -continueIfFileNotExist $continueIfFileNotExist
+Remove-FileFromWebApp -webAppName "$WebAppName" -username $login.Properties.PublishingUserName -password $pw -filePath "$filePath" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeKuduUrl -continueIfFileNotExist $continueIfFileNotExist -deleteRecursive $deleteRecursive
 Write-Output "File $filePath deleted"
---
+++ geertvdc-vsts-azurewebappvfs-delete/vfs.psm1
@@ -0,0 +1,127 @@
+
+function Get-AzureRmWebAppPublishingCredentials($resourceGroupName, $webAppName, $slotName = $null){
+	if ([string]::IsNullOrWhiteSpace($slotName)){
+		$resourceType = "Microsoft.Web/sites/config"
+		$resourceName = "$webAppName/publishingcredentials"
+	}
+	else{
+		$resourceType = "Microsoft.Web/sites/slots/config"
+		$resourceName = "$webAppName/$slotName/publishingcredentials"
+	}
+	$publishingCredentials = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -Action list -ApiVersion 2015-08-01 -Force
+    	return $publishingCredentials
+}
+
+function Get-KuduApiAuthorisationToken($username, $password){
+    return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password))))
+}
+
+function Get-FileListFromWebApp($webAppName, $slotName = "", $username, $password, $filePath, $allowUnsafe = $false, $alternativeUrl, $continueIfFileNotExist){
+
+    $kuduApiAuthorisationToken = Get-KuduApiAuthorisationToken $username $password
+    if ($slotName -eq ""){				
+        $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
+    }
+    else{
+        $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
+    }
+
+	if($alternativeUrl -ne ""){
+		$kuduApiUrl = $kuduApiUrl.Replace("scm.azurewebsites.net","$alternativeUrl")
+	}
+
+    if($allowUnsafe -eq $true){
+		add-type @"
+		using System.Net;
+		using System.Security.Cryptography.X509Certificates;
+			public class TrustAllCertsPolicy : ICertificatePolicy {
+			public bool CheckValidationResult(
+				ServicePoint srvPoint, X509Certificate certificate,
+				WebRequest request, int certificateProblem) {
+				return true;
+				}
+			}
+"@
+		[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+	}
+
+	try {
+    	$dirList = Invoke-RestMethod -Uri $kuduApiUrl `
+									 -Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} `
+									 -Method GET `
+									 -ContentType "multipart/form-data"		
+		return $dirList
+	}
+	catch {
+		if($_.Exception.Response.StatusCode.value__ -eq "404" -and $continueIfFileNotExist -eq $true){
+			Write-Output "File not found (but ignored because of setting)"
+		}
+		else {
+			throw $_.Exception
+		}
+	}
+}
+
+function Remove-FileFromWebApp($webAppName, $slotName = "", $username, $password, $filePath, $allowUnsafe = $false, $alternativeUrl, $continueIfFileNotExist, $deleteRecursive){
+
+	Write-Output "Remove-FileFromWebApp (user $username ,webapp $webAppName ,path $filePath ,allowunsafe $allowUnsafe ,deleteRecursive $deleteRecursive"
+	if($deleteRecursive -eq $true -and $filePath.EndsWith("/")){
+		
+		Write-Output "Recursive delete so Get-FileListFromWebApp to see which files to delete: $filePath"
+		$dirs = Get-FileListFromWebApp -webAppName "$webAppName" -username $username -password $password -filePath "$filePath" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeUrl -continueIfFileNotExist $continueIfFileNotExist
+		foreach($file in $dirs){
+			$href = $file.href
+			$filename = $href.Substring($file.href.IndexOf("/vfs/site/wwwroot/")+18)
+
+			Remove-FileFromWebApp -webAppName "$webAppName" -username $username -password $password -filePath "$filename" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeUrl -continueIfFileNotExist $continueIfFileNotExist -deleteRecursive $deleteRecursive
+		}
+
+		Remove-FileFromWebApp -webAppName "$webAppName" -username $username -password $password -filePath "$filename" -allowUnsafe $allowUnsafe -alternativeUrl $alternativeUrl -continueIfFileNotExist $continueIfFileNotExist -deleteRecursive $false
+
+		return
+	}
+
+    $kuduApiAuthorisationToken = Get-KuduApiAuthorisationToken $username $password
+    if ($slotName -eq ""){				
+        $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
+    }
+    else{
+        $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$filePath"
+    }
+
+	if($alternativeUrl -ne ""){
+			$kuduApiUrl = $kuduApiUrl.Replace("scm.azurewebsites.net","$alternativeUrl")
+	}
+
+	Write-Output "url $kuduApiUrl"
+
+    if($allowUnsafe -eq $true){
+	 		add-type @"
+	   		using System.Net;
+	   		using System.Security.Cryptography.X509Certificates;
+		 		public class TrustAllCertsPolicy : ICertificatePolicy {
+		   		public bool CheckValidationResult(
+			 		ServicePoint srvPoint, X509Certificate certificate,
+			 		WebRequest request, int certificateProblem) {
+			   		return true;
+			 		}
+		 		}
+"@
+		[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+	}
+
+	try {
+		Invoke-RestMethod -Uri $kuduApiUrl `
+						  -Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} `
+						  -Method DELETE `
+						  -ContentType "multipart/form-data"			
+	}
+	catch {
+		if($_.Exception.Response.StatusCode.value__ -eq "404" -and $continueIfFileNotExist -eq $true){
+			Write-Output "File not found (but ignored because of setting)"
+		}
+		else {
+			throw $_.Exception
+		}		
+	}
+}
